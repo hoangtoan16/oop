0fc28fde9cfa736e30cd11cac2a2e7292125d79d
hoangtoan16 <161427513+hoangtoan16@users.noreply.github.com>
2024-02-29 00:10:02 +0700

    Add files via upload

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.printf("HoangSyToan\t23020426\tK68-AI2\thoangtoan16\n");
+        for(int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.printf("No more bottles of beer on the wall.");
+    }
+}
